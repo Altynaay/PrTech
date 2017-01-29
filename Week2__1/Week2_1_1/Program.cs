d0e4d77a81a4b6bab526a42976e0e203112f26cc
Nurzhigit <nusmailov@gmail.com>
2017-01-29 08:56:12 +0600

    Working at files NeW

---
+++ Week2__1/Week2_1_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Example2
+{
+    class Program
+    {
+        
+        static void Fil()
+        {
+            DirectoryInfo directory = new DirectoryInfo(@"G:");// get information from G: disk
+            FileInfo[] files = directory.GetFiles();
+            DirectoryInfo[] directories = directory.GetDirectories();
+
+            Console.WriteLine("Files:");
+            foreach (FileInfo file in files)//cycle which write files names
+            {
+                Console.WriteLine(file.Name);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Directories:");
+            foreach (DirectoryInfo dInfo in directories)//cycle which write directories names
+            {
+                Console.WriteLine(dInfo.Name);
+            }
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            Fil();
+        }
+    }
+}
