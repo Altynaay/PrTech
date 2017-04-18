5e29d57325f7b48ccc1c2099af45142e1ee6e224
Nurzhigit <nusmailov@gmail.com>
2017-04-19 01:35:09 +0600

    ThreadMoving

---
+++ Tank/Tank/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+/*using System.Drawing.Bitmap;
+using System.Drawing.Graphics;
+using System.Drawing.Image;*/
+
+namespace Tank
+{
+    public partial class Form1 : Form
+    {
+        TankGame tanks = new TankGame();
+        Graphics gpic;
+        Bitmap bitmap;
+        Graphics bitG;
+        KeyEventArgs keypressed;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            tanks.MoveDown(e.Graphics);
+            //tanks.MoveUp(e.Graphics);
+            //tanks.MoveRight(e.Graphics);
+            //tanks.MoveLeft(e.Graphics);
+        }
+        private void DownKey(object sender, KeyEventArgs g, PaintEventArgs e)
+        {
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+        }
+
+    }
+}
---
+++ Tank/Tank/TankGame.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Threading;
+
+namespace Tank
+{
+    public class TankGame
+    {
+        public Pen pen = new Pen(Color.Black);
+        public Point location;
+        public int length = 40;
+        SolidBrush brushblack = new SolidBrush(Color.Black);
+        public void MoveUp(Graphics g)
+        {
+            while (location.Y != 0)
+            {
+                g.Clear(Color.White);
+                g.FillRectangle(brushblack, location.X + 30, location.Y + 30, length, length);
+                g.FillRectangle(brushblack, location.X + 40, location.Y + 10, length / 2, length);
+                location.Y--;
+                Thread.Sleep(10);
+            }
+        }
+        public void MoveDown(Graphics g)
+        {
+            while (location.Y != 390)
+            {
+                g.Clear(Color.White);
+                g.FillRectangle(brushblack, location.X + 30, location.Y + 30, length, length);
+                g.FillRectangle(brushblack, location.X + 40, location.Y + 50, length / 2, length);
+                location.Y++;
+                Thread.Sleep(10);
+            }
+        }
+        public void MoveRight(Graphics g)
+        {
+            while (location.X != 865)
+            {
+                g.Clear(Color.White);
+                g.FillRectangle(brushblack, location.X + 30, location.Y + 30, length, length);
+                g.FillRectangle(brushblack, location.X + 50, location.Y + 40, length, length / 2);
+                location.X++;
+                Thread.Sleep(10);
+            }
+        }
+        public void MoveLeft(Graphics g)
+        {
+            while (location.X != 0)
+            {
+                g.Clear(Color.White);
+                g.FillRectangle(brushblack, location.X + 30, location.Y + 30, length, length);
+                g.FillRectangle(brushblack, location.X + 10, location.Y + 40, length, length / 2);
+                location.X--;
+                Thread.Sleep(10);
+            }
+        }
+        
+
+    }
+}
