b28bcfe65ee967cf73189bd42bf80087d64d736d
Nurzhigit <nusmailov@gmail.com>
2017-04-19 02:17:20 +0600

    OneVSTwo

--- Tank/Tank/Form1.cs
+++ Tank/Tank/Form1.cs
@@ -17,7 +17,7 @@ namespace Tank
         Graphics gpic;
         Bitmap bitmap;
         Graphics G;
-        public int k = 1;
+        public int k = 1, s = 4;
         KeyEventArgs keypressed;
         public Form1()
         {
@@ -32,14 +32,7 @@ namespace Tank
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            if (k == 4)
-            {
-                tanks.MoveDown(e.Graphics);
-            }
-            if (k == 3)
-            {
-                tanks.MoveUp(e.Graphics);
-            }
+            //k
             if (k == 1)
             {
                 tanks.MoveRight(e.Graphics);
@@ -48,22 +41,39 @@ namespace Tank
             {
                 tanks.MoveLeft(e.Graphics);
             }
-        }
-        private void DownKey(object sender, KeyEventArgs g, PaintEventArgs e)
-        {
-            
+            if (k == 3)
+            {
+                tanks.MoveUp(e.Graphics);
+            }
+            if (k == 4)
+            {
+                tanks.MoveDown(e.Graphics);
+            }
+            //s
+            if (s == 1)
+            {
+                tanks.MoveRight2(e.Graphics);
+            }
+            if (s == 2)
+            {
+                tanks.MoveLeft2(e.Graphics);
+            }
+            if (s == 3)
+            {
+                tanks.MoveUp2(e.Graphics);
+            }
+            if (s == 4)
+            {
+                tanks.MoveDown2(e.Graphics);
+            }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-
+            //k
             if (k == 1)
             {
                 tanks.location.X += 10;
-            }
-            if (k == 4)
-            {
-                tanks.location.Y += 10;
             }
             if (k == 2)
             {
@@ -73,11 +83,33 @@ namespace Tank
             {
                 tanks.location.Y -= 10;
             }
+            if (k == 4)
+            {
+                tanks.location.Y += 10;
+            }
+            //s
+            if (s == 1)
+            {
+                tanks.location2.X += 10;
+            }
+            if (s == 2)
+            {
+                tanks.location2.X -= 10;
+            }
+            if (s == 3)
+            {
+                tanks.location2.Y -= 10;
+            }
+            if (s == 4)
+            {
+                tanks.location2.Y += 10;
+            }
             Refresh();
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
+            //k
             if (e.KeyCode == Keys.Right)
             {
                 k = 1;
@@ -93,6 +125,23 @@ namespace Tank
             if (e.KeyCode == Keys.Down)
             {
                 k = 4;
+            }
+            //s
+            if (e.KeyCode == Keys.D)
+            {
+                s = 1;
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                s = 2;
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                s = 3;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                s = 4;
             }
         }
 
--- Tank/Tank/TankGame.cs
+++ Tank/Tank/TankGame.cs
@@ -12,7 +12,7 @@ namespace Tank
     public class TankGame
     {
         public Pen pen = new Pen(Color.Black);
-        public Point location,lright,lleft,lup,ldown;
+        public Point location,lright,lleft,lup,ldown,location2;
         public int length = 40;
         SolidBrush brushblack = new SolidBrush(Color.Black);
         public void MoveUp(Graphics g)
@@ -34,7 +34,29 @@ namespace Tank
         {
             g.FillRectangle(brushblack, location.X + 30, location.Y + 30, length, length);
             g.FillRectangle(brushblack, location.X + 10, location.Y + 40, length, length / 2);
-             
+        }
+
+
+
+        public void MoveUp2(Graphics g)
+        {
+            g.FillRectangle(brushblack, location2.X + 30, location2.Y + 30, length, length);
+            g.FillRectangle(brushblack, location2.X + 40, location2.Y + 10, length / 2, length);
+        }
+        public void MoveDown2(Graphics g)
+        {
+            g.FillRectangle(brushblack, location2.X + 30, location2.Y + 30, length, length);
+            g.FillRectangle(brushblack, location2.X + 40, location2.Y + 50, length / 2, length);
+        }
+        public void MoveRight2(Graphics g)
+        {
+            g.FillRectangle(brushblack, location2.X + 30, location2.Y + 30, length, length);
+            g.FillRectangle(brushblack, location2.X + 50, location2.Y + 40, length, length / 2);
+        }
+        public void MoveLeft2(Graphics g)
+        {
+            g.FillRectangle(brushblack, location2.X + 30, location2.Y + 30, length, length);
+            g.FillRectangle(brushblack, location2.X + 10, location2.Y + 40, length, length / 2);
         }
     }
 }
